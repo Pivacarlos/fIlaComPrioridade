4f68fdf6ab22b9abb21b75232d9b6d6e55e778ae
Carlos Eduardo Piva <pivacarlos13@gmail.com>
2023-05-01 22:38:20 -0300

    adjust interface

--- filaComPrioridade.py
+++ filaComPrioridade.py
@@ -1,5 +1,6 @@
 import heapq
 import random
+import time
 
 fila_prioridades = []
 fila_comum = []
@@ -52,7 +53,8 @@ def main():
     tam_fila = 0
 
     while True:
-        print("---- MENU ----")
+        time.sleep(1.5)
+        print("\n---- MENU ----")
         print("1) Definir tamanho da fila com prioridades")
         print("2) Adicionar novo grupo na fila com prioridades")
         print("3) Mostrar próximo grupo aguardando")
@@ -62,45 +64,49 @@ def main():
         opcao = int(input("Escolha uma opção: "))
 
         if opcao == 1:
-            tam_fila = int(input("Digite o tamanho da fila de prioridades: "))
+            tam_fila = int(input("\nDigite o tamanho da fila de prioridades: "))
+            print("\n============================================\nTamanho da fila de prioridades definido para:", tam_fila,"\n============================================")
 
         elif opcao == 2:
             if len(fila_prioridades) < tam_fila:
-                nome = input("Digite o nome do grupo: ")
+                nome = input("\nDigite o nome do grupo: ")
                 qtd_pessoas = int(input("Digite a quantidade de pessoas: "))
                 tempo_preparo = int(input("Digite o tempo de preparo em minutos: "))
                 novo_grupo = (qtd_pessoas, tempo_preparo, nome)
                 heapq.heappush(fila_prioridades, novo_grupo)
+                print("\n==============================\nGrupo adicionado com sucesso!\n==============================")
+                        
+
             else:
-                print("A fila de prioridades já está lotada!")
+                print("\nA fila de prioridades já está lotada!\n")
 
         elif opcao == 3:
             if len(fila_prioridades) > 0:
                 proximo_grupo = heapq.heappop(fila_prioridades)
-                print("Próximo grupo aguardando:")
+                print("\nPróximo grupo aguardando:")
                 print("Nome:", proximo_grupo[2])
                 print("Quantidade de pessoas:", proximo_grupo[0])
-                print("Tempo de preparo:", proximo_grupo[1], "minutos")
+                print("Tempo de preparo:", proximo_grupo[1], "minutos\n")
                 heapq.heappush(fila_prioridades, proximo_grupo)
             else:
-                print("A fila de prioridades está vazia!")
+                print("\nA fila de prioridades está vazia!\n")
 
         elif opcao == 4:
             if len(fila_prioridades) > 0:
                 grupo_preparo = heapq.heappop(fila_prioridades)
                 tempo_espera = grupo_preparo[1] + sum([x[1] for x in fila_preparo])
                 fila_preparo.append(grupo_preparo)
-                print("Preparando próxima refeição para o grupo:", grupo_preparo[2])
-                print("Tempo estimado de espera:", tempo_espera, "minutos")
+                print("\nPreparando próxima refeição para o grupo:", grupo_preparo[2])
+                print("Tempo estimado de espera:", tempo_espera, "minutos \n")
             else:
-                print("A fila de prioridades está vazia!")
+                print("\nA fila de prioridades está vazia!\n")
 
         elif opcao == 5:
             if len(fila_preparo) > 0:
                 grupo_entrega = fila_preparo.pop(0)
-                print("Refeição entregue para o grupo:", grupo_entrega[2])
+                print("\n Refeição entregue para o grupo:", grupo_entrega[2] + "\n")
             else:
-                print("A fila de preparo está vazia!")
+                print("\n A fila de preparo está vazia!\n")
 
         elif opcao == 6:
             break
